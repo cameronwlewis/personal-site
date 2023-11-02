765baa68b5d5e4281e01b335d0556eea68d7a5d9
Cameron Lewis <cameron@pitch59.com>
2023-11-02 15:25:04 -0600

    added expertise page route

---
+++ app/expertise/page.tsx
@@ -0,0 +1,13 @@
+import Menu from "@/components/Menu";
+
+export default function Expertise() {
+  return (
+    <main className="text-black">
+      <Menu textColor={"text-black"}/>
+      <br/>
+      <br/>
+      <br/>
+      This page is under construction folks.
+    </main>
+  );
+}
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,15 +1,17 @@
-const Menu = () => (
-  <div className="grid grid-cols-5 font-mono text-white pt-5">
+import Link from "next/link";
+
+const Menu = ({ textColor = "text-white" }) => (
+  <div className={`grid grid-cols-5 font-mono pt-5 ${textColor}`}>
     {/* eslint-disable-next-line */}
-    <div>// home</div>
+    <Link href={"/"}>// home</Link>
     {/* eslint-disable-next-line */}
-    <div>// expertise</div>
+    <Link href={"/expertise"}>// expertise</Link>
     {/* eslint-disable-next-line */}
-    <div>// work</div>
+    <div className="text-gray-400">// work</div>
     {/* eslint-disable-next-line */}
-    <div>// experience</div>
+    <div className="text-gray-400">// experience</div>
     {/* eslint-disable-next-line */}
-    <div>// contact</div>
+    <div className="text-gray-400">// contact</div>
   </div>
 );
 
