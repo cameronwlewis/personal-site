c87b00883058ab3a2e2be0451e82509c74951beb
Cameron Lewis <cameron@pitch59.com>
2023-11-04 15:08:24 -0600

    introduced daisyUI; responsive menu

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import logo from "../assets/cameron-logo.svg";
 import Menu from "@/components/Menu";
 import Timeline from "@/components/Timeline";
 
-
 export default function Home() {
   return (
     <main className="bg-black ">
@@ -11,7 +10,7 @@ export default function Home() {
         <div className="bg-gradient-to-b from-black from-2% to-transparent to-10% ">
           <div className="h-screen container mx-auto ">
             <Menu />
-            <div className="mt-40 flex justify-center lg:flex-none lg:justify-end lg:mr-52 lg:mt-52">
+            <div className="pt-52 flex justify-center md:pt-52 lg:flex-none lg:justify-end lg:mr-52">
               <div className="lg:float-right">
                 <Image src={logo} alt={"cameron lewis logo"} />
               </div>
